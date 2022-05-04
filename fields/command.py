8cabca4871cd1c52ef6cb129572efbd789dbb59c
Alexander Lakeev <alakeev@gmail.com>
2022-05-04 16:45:26 +0300

    Refactor with new SDK

--- fields/command.py
+++ fields/command.py
@@ -1,15 +1,13 @@
-from pp_exec_env.base_command import BaseCommand, Syntax, Rule, pd
+import pandas as pd
+from pp_exec_env.base_command import BaseCommand, Syntax
+from otlang.sdk.syntax import Positional, OTLType
 
 
 class FieldsCommand(BaseCommand):
-    """Fields command with sequestered logics. To be improved. """
+    """Fields command with sequestered logics. To be improved."""
 
-    syntax = Syntax(
-        [Rule(name="field", type="arg", input_types=['string', 'term'], inf=True, )],  # must_be_a_field=True
-        use_timewindow=False
-    )
+    syntax = Syntax([Positional("field", otl_type=OTLType.TEXT, inf=True)], use_timewindow=False)
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         fields = self.get_iter('col')
-
         return df[[field.value for field in fields]]
